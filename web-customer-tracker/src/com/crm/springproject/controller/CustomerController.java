3a515593c823d174e72d8c0a4f3a826e22ff8905
Andres Andrade <a.andrade09@yahoo.com>
2018-08-29 21:42:23 -0700

    Added customer DAO to customer controller.
    Added customer DAO with auto wired annotation for injection.
    Updated listCustomers() method.

--- web-customer-tracker/src/com/crm/springproject/controller/CustomerController.java
+++ web-customer-tracker/src/com/crm/springproject/controller/CustomerController.java
@@ -1,17 +1,34 @@
 package com.crm.springproject.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.crm.springproject.dao.CustomerDAO;
+import com.crm.springproject.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	// need to inject customer DAO
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
-	public String listCustomers(Model theModel) {
+	public String listCustomers(Model model) {
+		// get customers from DAO
+		List<Customer> customers = customerDAO.getCustomers();
+		
+		// add customers to model
+		model.addAttribute("customers", customers);
+		
 		return "list-customers";
 	}
 	
-
+	
+	
 }
