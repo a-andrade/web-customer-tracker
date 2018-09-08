b605fcf90733bb7d15ec8edd440b26e204792177
Andres Andrade <a.andrade09@yahoo.com>
2018-09-07 19:40:08 -0700

    Updated customer controller.
    Added methods for updating/deleting a customer.

--- web-customer-tracker/src/com/crm/springproject/controller/CustomerController.java
+++ web-customer-tracker/src/com/crm/springproject/controller/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.crm.springproject.entity.Customer;
 import com.crm.springproject.service.CustomerService;
@@ -46,6 +47,27 @@ public class CustomerController {
 	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
 		// save the customer using our service
 		customerService.saveCustomer(theCustomer);
+		
+		return "redirect:/customer/list";
+	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("customerId") int theId,
+									 Model theModel) {
+		// get the customer from the database
+		Customer theCustomer = customerService.getCustomer(theId);
+		
+		// set the customer as a model attribute to pre-populate the form
+		theModel.addAttribute("customer", theCustomer);
+		
+		// send over to our form
+		return "customer-form";
+	}
+	
+	@GetMapping("/deleteCustomer")
+	public String deleteCustomer(@RequestParam("customerId") int theId) {
+		// delete customer
+		customerService.deleteCustomer(theId);
 		
 		return "redirect:/customer/list";
 	}
